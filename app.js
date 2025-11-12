459d43afc8191874b219d1b752ead77ea0200cf5
HaroldOE <harold.ekempka@speedlinkng.com>
2025-11-12 02:27:04 +0100

    updated

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express";
 import router from "./routes/userRoutes.js";
+
+// import endpoint
+import userRouter from "./routes/userRoutes.js";
 const app = express();
 app.use(express.json());
 const PORT = 3000;
 
+// mount endpoints
+app.use("/api/")
 
 app.use("/api/user", router);
 
